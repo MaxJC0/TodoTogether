f5de74a330e5d598804017dd5231a48395600ece
ahalangk <halangk.arne@gmail.com>
2025-10-29 12:31:50 +0100

    added layout for friends tab, not done yet

--- my_app/app/(tabs)/profile.tsx
+++ my_app/app/(tabs)/profile.tsx
@@ -1,40 +1,71 @@
-import React, { useState } from 'react';
-import { StyleSheet } from 'react-native';
-import { ThemedText } from '@/components/themed-text';
-import { ThemedView } from '@/components/themed-view';
-import { RadioGroup } from '@/components/ui/radio';
+import { ThemedText } from "@/components/themed-text";
+import { ThemedView } from "@/components/themed-view";
+import React from 'react';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
 
-export default function TestScreen() {
-  const [priority, setPriority] = useState<string | null>('medium');
 
-  return (
-    <ThemedView style={styles.container}>
-      <ThemedText type="title">Choose priority</ThemedText>
 
-      <RadioGroup
-        value={priority}
-        onChange={setPriority}
-        direction="vertical"
-        options={[
-          { label: 'Low', value: 'low' },
-          { label: 'Medium', value: 'medium' },
-          { label: 'High', value: 'high' },
-        ]}
-      />
+export default function DummyList() {
 
-      <ThemedText style={{ marginTop: 12 }}>
-        Selected: {priority ?? 'none'}
-      </ThemedText>
-    </ThemedView>
+const testTasks = [
+ 
+  { id: "1", name: 'Alice Anderson', username: 'a_anderson' },
+  { id:"2", name: 'Bob Berger', username: 'b_berger' },
+  { id: "3", name: 'Charlie Chaplin', username: 'charlie' },
+  { id: "4", name: 'David Dinkel', username: 'dave' },
+  { id: "5", name: 'Eva Erlich', username: 'eva_e' },
+  { id: "6", name: 'Frank Fuchs', username: 'franky' },
+  { id: "7", name: 'Greta Gerwig', username: 'greta' },
+  { id: "8", name: 'Hans Haas', username: 'h_haas' },
+  { id: "9", name: 'Ida Igel', username: 'ida' },
+  { id: "10", name: 'Alice Anderson', username: 'a_anderson' },
+  { id:"11", name: 'Bob Berger', username: 'b_berger' },
+  { id: "12", name: 'Charlie Chaplin', username: 'charlie' },
+  { id: "13", name: 'David Dinkel', username: 'dave' },
+  { id: "14", name: 'Eva Erlich', username: 'eva_e' },
+  { id: "15", name: 'Frank Fuchs', username: 'franky' },
+  { id: "16", name: 'Greta Gerwig', username: 'greta' },
+  { id: "17", name: 'Hans Haas', username: 'h_haas' },
+  { id: "18", name: 'Ida Igel', username: 'ida' },
+];
+
+// jedes flatlist element hat item eigenschaft, item wird zum rendern übergeben (für jedes element) 
+// style = {styles.item} = style für jedes item = entry, ist von unten; item.name ist aus dem oberen array, nicht verwechseln!
+/// flatlist item braucht 
+// 1. data 
+// 2 . key(extractor) 
+// 3. renderItem
+return(
+    <ThemedView style={styles.Container}>
+      <ThemedText type="title">archive</ThemedText>     
+      <FlatList
+          data = {testTasks}  
+          keyExtractor={ item => item.id}
+          renderItem={({ item }) => (
+            <View style= {styles.item}>
+              <Text>{item.name}</Text>
+            </View>
+            )}
+          />
+      </ThemedView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  Container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    gap: 12,
-    paddingHorizontal: 24,
+    justifyContent: "center",
+    backgroundColor: "rgba(0, 0, 0, 0)",
+  },
+  item: {
+    padding: 12,
+    marginVertical: 5,
+    backgroundColor: "#6e646478",
+    borderRadius: 8,
+    width: "100%",      
+
   },
 });
+
+
+
